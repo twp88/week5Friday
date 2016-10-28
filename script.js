342874070fb124077fd1af0d721b509c99d07ff9
Tom Page <tom.w.page@gmail.com>
2016-10-28 11:26:51 +0100

    this should be everything

--- script.js
+++ script.js
@@ -2,17 +2,16 @@ $(document).ready(function() {
 
   var thermostat = new Thermostat();
   updateTemperature();
+  boxColour();
 
   $("#power-saving-status").text(thermostat.returnMode());
 
   $("#powerModeOff").click(function() {
     thermostat.powerModeOff();
-
   $("#power-saving-status").text(thermostat.returnMode());
 });
 
   $("#powerModeOn").click(function() {
-    //console.log("Hello")
     thermostat.powerModeOn();
   $("#power-saving-status").text(thermostat.returnMode());
   updateTemperature();
@@ -21,21 +20,34 @@ $(document).ready(function() {
   $("#increaseTemperature").click(function() {
     thermostat.increaseTemperature();
     updateTemperature();
+    boxColour();
 });
 
   $("#decreaseTemperature").click(function() {
         thermostat.decreaseTemperature();
         updateTemperature();
+        boxColour();
 });
 
   $("#resetTemperature").click(function() {
     thermostat.resetTemperature();
     updateTemperature();
+    boxColour();
   });
 
   function updateTemperature() {
     $('#temperature').text(thermostat.returnTemperature());
     $("#usage").text(thermostat.tellUsUsage());
+  };
+
+  function boxColour() {
+    if (thermostat.tellUsUsage() === "low-usage") {
+      $('#box').css('background-color', 'blue');
+    } else if (thermostat.tellUsUsage() === "medium-usage") {
+      $('#box').css('background-color', 'green');
+    } else if (thermostat.tellUsUsage() === "high-usage") {
+      $('#box').css('background-color', 'red');
+  }
   };
 
 });
